abfe586a1b246f974f0e74586f443fdc11aba49a
poisson0106 <poisson0106@gmail.com>
2016-08-05 17:13:15 +0800

    Dao structure change

--- src/main/java/com/sjw/ShiroTest/DaoImpl/AuthDaoImpl.java
+++ src/main/java/com/sjw/ShiroTest/DaoImpl/AuthDaoImpl.java
@@ -1,18 +1,20 @@
 package com.sjw.ShiroTest.DaoImpl;
 
-import org.apache.ibatis.session.SqlSessionFactory;
-import org.mybatis.spring.support.SqlSessionDaoSupport;
+import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import com.sjw.ShiroTest.Dao.AuthDao;
 import com.sjw.ShiroTest.Pojo.UserPojo;
 
-public class AuthDaoImpl extends SqlSessionDaoSupport implements AuthDao {
+@Repository
+public class AuthDaoImpl implements AuthDao {
+	@Autowired
+	SqlSession sqlSession;
 	
 	@Override
 	public void registerUserDao(UserPojo user) {
-		this.getSqlSession().insert("createUser",user);
+		this.sqlSession.insert("createUser",user);
 	}
 
 }
